3943d5cb46e5a0d81da54693a0cafcaac2f5689e
Javey Su <Javey.Su@gmail.com>
2026-03-11 02:34:08 +0000

    How to turn this app program to an apk that can be install on an android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.beatbuddy.app',
+  appName: 'BeatBuddy',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
